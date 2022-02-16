0fbfd63db7b4325d4127bcbc9f2639f16ca8e4a9
MingsonZheng <MingsonZheng@outlook.com>
2022-02-16 17:45:41 +0800

    WeatherForecastController add Authorize

--- src/DotNetNB.WebApplication/Controllers/WeatherForecastController.cs
+++ src/DotNetNB.WebApplication/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetNB.WebApplication.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
